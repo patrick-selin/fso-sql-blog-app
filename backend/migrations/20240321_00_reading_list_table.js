1143caaa8627bd78db5e64b7a3afec6cb65309ac
Patrick Selin <patrick.selin@gmail.com>
2024-03-21 09:19:21 +0200

    13.19 readinglist migration file

---
+++ backend/migrations/20240321_00_reading_list_table.js
@@ -0,0 +1,37 @@
+// miggation here
+const { DataTypes } = require("sequelize");
+
+module.exports = {
+  up: async ({ context: queryInterface }) => {
+    await queryInterface.createTable("reading_lists", {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: "users", key: "id" },
+      },
+      blog_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: "blogs", key: "id" },
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    });
+  },
+  down: async ({ context: queryInterface }) => {
+    await queryInterface.dropTable("reading_list");
+  },
+};
