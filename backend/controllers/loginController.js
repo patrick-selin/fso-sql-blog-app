f0fabe6d71d7d541649aab8c5a0f20f20dc81ea6
Patrick Selin <patrick.selin@gmail.com>
2024-03-26 10:56:17 +0200

    13.24 POST /api/login session handeling

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const bcryptjs = require("bcryptjs");
 const loginRouter = require("express").Router();
 const User = require("../models/user");
+const Session = require("../models/session");
 
 const LONG_TIME = 60 * 60 * 600;
 
@@ -19,9 +20,21 @@ loginRouter.post("/", async (req, res) => {
 
   if (!(user && passwordCorrect)) {
     return res.status(401).json({
-      error: "invalid username or password",
+      error: "Invalid username or password",
     });
   }
+  if (user.disabled) {
+    return res.status(401).json({
+      error: "Disabled user - no access",
+    });
+  }
+
+  console.log(`this is ID ::  ${JSON.stringify(user.id)}`);
+  await Session.destroy({
+    where: {
+      userId: user.id,
+    },
+  });
 
   const userForToken = {
     username: user.username,
@@ -32,7 +45,12 @@ loginRouter.post("/", async (req, res) => {
     expiresIn: LONG_TIME,
   });
 
-  // console.log(`CLG ====${JSON.stringify(token)}`);
+  await Session.create({
+    userId: user.id,
+    token: token,
+    active: true,
+  });
+
   res
     .status(200)
     .send({ token, username: user.username, name: user.name, id: user.id });
